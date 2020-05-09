d55559100b557e106f61af3ecf553efa971d7de8
Eda Tuncer <edatezcanli@gmail.com>
2020-05-09 09:43:02 -0700

    test

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
-var apiId = "e511eb05b61dbc2e4319bdd390f1a012"; 
+// var apiId = "e511eb05b61dbc2e4319bdd390f1a012"; 
 var ingredient = ""; 
-var recipe =""
+var recipe ="";
 
 
 
 function searchRecipes (ingredient){  
 
   // Server Interaction
-      var queryURL= `https://api.edamam.com/search?q=chicken&app_id=${e511eb05b61dbc2e4319bdd390f1a012}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+      var queryURL= `https://api.edamam.com/search?q=chicken&app_id=8c8c5fc5&app_key=44f93eddda07342de4a29d012aa7aa08&from=0&to=3&calories=591-722&health=alcohol-free`
       console.log(queryURL)
       $.ajax({
           url: queryURL,
@@ -17,7 +17,5 @@ function searchRecipes (ingredient){
   
          response(ingredient);
       });
-  }
+  };
   searchRecipes (ingredient);
-
-  
