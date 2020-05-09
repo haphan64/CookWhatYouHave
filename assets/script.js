9b5a83f2fb0d3c5fdb98b209bf849f2dee36737e
Eda Tuncer <edatezcanli@gmail.com>
2020-05-09 14:00:16 -0700

    updated the script file

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,61 @@
-// var apiId = "e511eb05b61dbc2e4319bdd390f1a012"; 
-var ingredient = ""; 
+var apiId = "6d15138b427645f68ff32030ae3cb1cc"; 
+// var apiId ="b229212145ae4030923cb4146500d590";
+// var apiId ="023b6d102d964dadb8821194c30016d7";
+
+var ingredients = "apples"; 
 var recipe ="";
+// var RecipeStore= JSON.parse(localStorage.getItem('recipe'));
+//  2nd end poi8nt; Get Recipe Information: https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
 
 
-
-function searchRecipes (ingredient){  
+function searchRecipes (ingredients){  
 
   // Server Interaction
-      var queryURL= `https://api.edamam.com/search?q=chicken&app_id=8c8c5fc5&app_key=44f93eddda07342de4a29d012aa7aa08&from=0&to=3&calories=591-722&health=alcohol-free`
-      console.log(queryURL)
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-      }).then(function (response) {
-         console.log(response);
-  
-         response(ingredient);
-      });
-  };
-  searchRecipes (ingredient);
+  var queryURL= "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredients + "&apiKey=" + apiId;
+  console.log(queryURL)
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).then(function (response) {
+     console.log(response);   
+     
+
+     getRecipe(response[0].id);
+     
+    //  loop for 
+    // listing the used ing
+for (i=0; i<response.length; i++){
+    console.log(response[i].title)
+    console.log(response[i].image);
+    // console.log(response[i].usedIngredients[0].name);
+    // console.log(response[i].missedIngredients[0].name);
+    console.log(response[i].id);
+}
+// another renderResult function within the for loop 
+//  create cards/html elements to put the recipe informations. 
+// clean up each part before . 
+// appending all the results to html. 
+// getRecipe function. 
+  });
+};
+
+searchRecipes (ingredients);
+
+function getRecipe(id){
+    var queryURL="https://api.spoonacular.com/recipes/" +id + "/information?includeNutrition=false"+ ingredients + "&apiKey=" + apiId;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log (response);
+        console.log (response.title)
+        console.log(response.image)
+        console.log(response.sourceUrl)
+        console.log(response.instructions)
+        console.log(response.analyzedInstructions[0].steps[0].ingredients)
+    });
+    
+}
+
+// only when we click we can the id. We need another function. When we click on the result. 
+
