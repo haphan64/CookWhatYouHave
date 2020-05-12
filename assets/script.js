8c4feeb28e912017451887809ea83b104ca5804c
Eda Tuncer <edatezcanli@gmail.com>
2020-05-11 20:52:59 -0700

    working on the second page showing results

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,15 @@ var apiId = "6d15138b427645f68ff32030ae3cb1cc";
 var ingredients = "apples"; 
 var recipe ="";
 // var RecipeStore= JSON.parse(localStorage.getItem('recipe'));
-//  2nd end poi8nt; Get Recipe Information: https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
+//  2nd end point; Get Recipe Information: https://api.spoonacular.com/recipes/716429/information?includeNutrition=false
+
+if (!recipe){
+    recipe =[]; 
+}
 
 
 function searchRecipes (ingredients){  
 
-  // Server Interaction
   var queryURL= "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredients + "&apiKey=" + apiId;
   console.log(queryURL)
   $.ajax({
@@ -18,19 +21,42 @@ function searchRecipes (ingredients){
       method: "GET"
   }).then(function (response) {
      console.log(response);   
-     
-
+// aray of recipes
+   
      getRecipe(response[0].id);
      
-    //  loop for 
-    // listing the used ing
+    //  loop for listing the used ingredients 
+    // for recipe id.
+    $("#show").empty() 
 for (i=0; i<response.length; i++){
     console.log(response[i].title)
-    console.log(response[i].image);
+
+    var recipeCard='<div class="card col-3" style="width: 18rem;"><img src='+ response[i].image +  ' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +response[i].title  +'</h5><p class="card-text">.</p><a href="#" class="btn btn-primary viewRecipe" recipe= '+response[i].id + '>View Recipe</a></div></div>'
+// ${}
+// var ..
+// append
+// add a style to the class
+  
+// var cardRecipe='<div>'
+// cardRecipe.addClass=""
+// var img ="<img>"
+// img.attr("src", respons[i).image
+//     var title
+
+    $("#show").append(recipeCard)
+   // console.log(response[i].image);
     // console.log(response[i].usedIngredients[0].name);
     // console.log(response[i].missedIngredients[0].name);
-    console.log(response[i].id);
+  //  console.log(response[i].id);
+    
+  //  getRecipe(response[i].id);
 }
+$(".viewRecipe").on ("click", function(){
+    console.log(this)
+    let id = $(this).attr("recipe")
+
+    getRecipe(id);
+})
 // another renderResult function within the for loop 
 //  create cards/html elements to put the recipe informations. 
 // clean up each part before . 
@@ -42,19 +68,20 @@ for (i=0; i<response.length; i++){
 searchRecipes (ingredients);
 
 function getRecipe(id){
-    var queryURL="https://api.spoonacular.com/recipes/" +id + "/information?includeNutrition=false"+ ingredients + "&apiKey=" + apiId;
+    var queryURL="https://api.spoonacular.com/recipes/" +id + "/information?includeNutrition=false" + "&apiKey=" + apiId;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log (response);
-        console.log (response.title)
-        console.log(response.image)
-        console.log(response.sourceUrl)
-        console.log(response.instructions)
-        console.log(response.analyzedInstructions[0].steps[0].ingredients)
-    });
-    
+        // console.log (response.title)
+        // console.log(response.image)
+        // console.log(response.sourceUrl)
+         console.log(response.instructions)
+
+         // go to a detail page or open a modal with the detail info
+        // console.log(response.analyzedInstructions[0].steps[0].ingredients)
+    });   
 }
 
 // only when we click we can the id. We need another function. When we click on the result. 
